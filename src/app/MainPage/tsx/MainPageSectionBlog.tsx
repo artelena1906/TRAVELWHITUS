68ecc30ca75bf2e03deede31a32cb1307c26ac96
lena <ae.petrovna@gmail.com>
2025-04-17 15:31:57 +0300

    Исправления

--- src/app/MainPage/tsx/MainPageSectionBlog.tsx
+++ src/app/MainPage/tsx/MainPageSectionBlog.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import styles from "../css/MainPageSectionBlog.module.css";
-import Link from "next/link";
-import Image from "next/image";
 
 export default function MainPageSectionBlog() {
     return (
