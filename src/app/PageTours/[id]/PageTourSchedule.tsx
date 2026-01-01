acb6cb8bc6e1383f0440d1010ea8a010e28f8433
artelena1906 <artelena@ukr.net>
2026-01-01 15:41:17 +0200

    сделаны правки страницы тура

--- src/app/PageTours/[id]/PageTourSchedule.tsx
+++ src/app/PageTours/[id]/PageTourSchedule.tsx
@@ -18,46 +18,45 @@ const formatDate = (dateString: string) => {
 };
 
   return (
-    <div className={styles.container}>
-      {tour.days.map((day, index) => (
-        <div key={day.id}>
-          <div className={styles.containerschedule}>
-            <div className={styles.leftColumn}>
-              <span className={styles.pDay}>День {day.dayNumber}</span>
-              <div className={styles.date}>{formatDate(day.date)}</div>
-            </div>
-            <div className={styles.rightColumn}>
-              {day.description
-                .split("\n")
-                .filter(p => p.trim() !== "")
-                .map((paragraph, i) => (
-                  <p key={i}>{paragraph}</p>
-                ))}
-            </div>
+  <div className={styles.container}>
+    {tour.days.map((day, index) => (
+      <div key={day.id}>
+        <div className={styles.containerschedule}>
+          <div className={styles.leftColumn}>
+            <span className={styles.pDay}>День {day.dayNumber}</span>
+            <div className={styles.date}>{formatDate(day.date)}</div>
           </div>
 
-          {day.photos.length > 0 && (
-            <div className={styles.photos}>
-              {day.photos.map((photo, idx) => (
-                <Image
-                  key={idx}
-                  src={photo}
-                  width={280}
-                  height={210}
-                  className={styles.dayImage}
-                  priority={false}
-                  alt={`Фото ${day.dayNumber}`}
-                />
+          <div className={styles.rightColumn}>
+            {day.description
+              .split("\n")
+              .filter(p => p.trim() !== "")
+              .map((paragraph, i) => (
+                <p key={i}>{paragraph}</p>
               ))}
-            </div>
-          )}
 
-          {/* Горизонтальная линия отдельно от flex-контейнера */}
-          {index < tour.days.length - 1 && (
-            <hr className={styles.separator} />
-          )}
+            {day.photos?.length > 0 && (
+              <div className={styles.photos}>
+                {day.photos.map((photo, idx) => (
+                  <Image
+                    key={idx}
+                    src={photo}
+                    width={280}
+                    height={210}
+                    className={styles.dayImage}
+                    alt={`Фото ${day.dayNumber}`}
+                  />
+                ))}
+              </div>
+            )}
+          </div>
         </div>
-      ))}
-    </div>
-  );
+
+        {index < tour.days.length - 1 && (
+          <hr className={styles.separator} />
+        )}
+      </div>
+    ))}
+  </div>
+);
 }
