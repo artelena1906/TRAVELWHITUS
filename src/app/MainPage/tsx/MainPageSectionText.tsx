0769048856889e90a6d87ba6eeb681b12b2170dd
artelena1906 <artelena@ukr.net>
2026-06-09 14:43:52 +0300

    Исправлен блок Про нас

--- src/app/MainPage/tsx/MainPageSectionText.tsx
+++ src/app/MainPage/tsx/MainPageSectionText.tsx
@@ -10,11 +10,11 @@ export default function MainPageSectionText() {
                 <Image src="/img/Baner.png"
                     alt="Image"
                     className={styles.sectionImage}
-                    width={550}
-                    height={450} />
+                    width={800}
+                    height={600} />
             </div>
             <div className={styles.textSection}>
-                <h3 className={styles.textTitle}>Кожен маршрут - моя ідея, Ваша пригода.</h3>
+                <h3 className={styles.textTitle}>Кожен маршрут - <br/>моя ідея, ваша пригода.</h3>
                 <span className={styles.textContainerP2}>Все почалося з бажання втекти від шаблонних «все включено»
                     та нудних гідів. Я шукала справжність, драйв і маршрути, які не знайти в путівниках.
                     Так з’явилася «Мандроманія».</span>
@@ -24,24 +24,10 @@ export default function MainPageSectionText() {
                 <span className={styles.textContainerP2}>
                     Головне правило: кожен маршрут - унікальний. Ми проживаємо його один раз і більше, як правило, не повторюємо. Наступного разу будуть нові країни та нові емоції.
                 </span>
-                <ul>
-                    Чому це круто:
-                    <li>
-                        <b>Більше можливостей:</b> Авто-тури дозволяють зазирнути туди, куди не дістаються звичайні туристи.
-                    </li>
-                    <li>
-                        <b>Перезавантаження:</b> Концентрований відрив від буднів, коли потрібно змінити картинку за лічені дні.
-                    </li>
-                    <li>
-                        <b>Справжній драйв:</b> Повне злиття з природою та затишок маленьких тихих містечок, де відчувається справжнє життя.
-                    </li>
-                    <li>
-                        <b>Гнучкість:</b> Індивідуальний підхід до кожної деталі, ритму та стилю подорожі.
-                    </li>
-                </ul>
-                <span className={styles.textContainerP2}>
+                
+                {/* <span className={styles.textContainerP2}>
                     Шукай свою пригоду в <Link href="/PageTours" className={styles.linkButton}>календарі турів</Link>. Погнали з нами, поки цей маршрут ще актуальний!
-                </span>
+                </span> */}
             </div>
         </div>
     )
